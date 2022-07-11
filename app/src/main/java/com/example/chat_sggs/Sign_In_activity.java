c0dc72adca88ab654eee84dea2124f2ea0415a58
Dhanshri Gatthawar <2020bcs106@sggs.ac.in>
2022-07-11 14:22:49 +0530

    Done it by Dhanshri

--- app/src/main/java/com/example/chat_sggs/Sign_In_activity.java
+++ app/src/main/java/com/example/chat_sggs/Sign_In_activity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Sign_In_activity extends AppCompatActivity {
+public class Sign_In_activity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
     }
